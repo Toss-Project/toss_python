e33eb2f6c1bc81deb85fa46081f6fc91a081bb4c
hsjung <hyunsujung3679@gmail.com>
2024-07-09 17:33:19 +0900

    이미지 업로드 시, 사진 설명 API 추가

--- image_captioning_hsj.py
+++ image_captioning_hsj.py
@@ -1,36 +1,39 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, UploadFile
+from transformers import VisionEncoderDecoderModel, ViTImageProcessor, AutoTokenizer
+import torch
+from PIL import Image
+import io
 
-# STEP 1: Import the necessary modules.
-import mediapipe as mp
-from mediapipe.tasks import python
-from mediapipe.tasks.python.components import processors
-from mediapipe.tasks.python import vision
+model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+feature_extractor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
 
-# STEP 2: Create an ImageClassifier object. # 추론기 객체 만들기
-base_options = python.BaseOptions(model_asset_path='models\image\efficientnet_lite0.tflite') # 모델 경로를 넣어준다.
-options = vision.ImageClassifierOptions(
-    base_options=base_options, max_results=1)
-classifier = vision.ImageClassifier.create_from_options(options)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
 
 app = FastAPI()
 
-from PIL import Image
-import numpy as np
-import io
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile):
     content = await file.read()
 
-    # STEP 3: Load the input image.
-    binary = io.BytesIO(content)
-    pil_img = Image.open(binary)
-    image = mp.Image(image_format=mp.ImageFormat.SRGB, data=np.asarray(pil_img))
+    # 바이트를 PIL 이미지로 변환
+    pil_img = Image.open(io.BytesIO(content)).convert("RGB")
 
-    # STEP 4: Classify the input image.
-    classification_result = classifier.classify(image)
+    # 분류 결과 처리
+    return predict_step([pil_img])
 
-    # STEP 5: Process the classification result. In this case, visualize it.
-    top_category = classification_result.classifications[0].categories[0]
-    result = f"{top_category.category_name} ({top_category.score:.2f})"
+max_length = 16
+num_beams = 4
+gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
 
-    return {"result": result}
+def predict_step(images):
+    pixel_values = feature_extractor(images=images, return_tensors="pt").pixel_values
+    pixel_values = pixel_values.to(device)
+
+    output_ids = model.generate(pixel_values, **gen_kwargs)
+
+    preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
+    preds = [pred.strip() for pred in preds]
+
+    return preds
