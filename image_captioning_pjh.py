a11264b5bda1adad7c35e8f906d71cf3e7ce8482
pjh990909 <pjh990909@naver.com>
2024-07-09 21:59:28 +0900

    ai 이미지 설명 텍스트 변환 2

--- image_captioning_pjh.py
+++ image_captioning_pjh.py
@@ -19,22 +19,22 @@ async def simulate_image_description(file: UploadFile):
     model = ollama.Client()
     
     # Bakllava 모델에 이미지와 프롬프트 전송
-    prompt = "Express this image in one sentence in English that is useful for TOEIC speaking."
+    prompt = "이 이미지를 다른 설명방식으로 3번 영어로 한 문장으로 설명해줘"
     response1 = model.generate(
         model='llava:7b',
         prompt=prompt,
         images=[image_base64]
     )
-    response2 = model.generate(
-        model='llava:7b',
-        prompt=prompt,
-        images=[image_base64]
-    )
-    response3 = model.generate(
-        model='llava:7b',
-        prompt=prompt,
-        images=[image_base64]
-    )
+    # response2 = model.generate(
+    #     model='llava:7b',
+    #     prompt=prompt,
+    #     images=[image_base64]
+    # )
+    # response3 = model.generate(
+    #     model='llava:7b',
+    #     prompt=prompt,
+    #     images=[image_base64]
+    # )
     # for i in range(3):
     #     if response1['response'] == response2['response']:
     #         response2 = await model.generate(
@@ -43,8 +43,10 @@ async def simulate_image_description(file: UploadFile):
     #         images=[image_base64]
     #         )
     
-    response_total=[response1['response'],response2['response'],response3['response']]
-    return response_total
+    # response_total=[response1['response'],response2['response'],response3['response']]
+    # return response_total
+
+    return response1['response']
 
 # 이미지 설명을 시뮬레이션합니다 (실제로는 이미지 분석 모델의 출력일 것입니다)
 # image_description = "A red apple sitting on a wooden table next to an open book."
