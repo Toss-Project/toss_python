29a9d2d8b4c62acfac30eda720f480b13fce8cb8
pjh990909 <pjh990909@naver.com>
2024-07-09 19:08:10 +0900

    Reapply "ai 이미지 설명 ollama버전"
    This reverts commit e9ab7ad1c92c1682ae3bbb34653a7bd03feb88a3.

---
+++ image_captioning_pjh.py
@@ -0,0 +1,55 @@
+# STEP 1
+from fastapi import FastAPI, File, UploadFile
+import ollama
+import base64 #표준 라이브러리에 있음
+
+# STEP 2
+
+app = FastAPI()
+
+@app.post("/uploadfile/")
+async def simulate_image_description(file: UploadFile):
+    
+    
+    contents = await file.read()
+    
+    # 이미지를 base64로 인코딩
+    image_base64 = base64.b64encode(contents).decode('utf-8')
+    
+    model = ollama.Client()
+    
+    # Bakllava 모델에 이미지와 프롬프트 전송
+    prompt = "Express this image in one sentence in English that is useful for TOEIC speaking."
+    response1 = model.generate(
+        model='llava:7b',
+        prompt=prompt,
+        images=[image_base64]
+    )
+    response2 = model.generate(
+        model='llava:7b',
+        prompt=prompt,
+        images=[image_base64]
+    )
+    response3 = model.generate(
+        model='llava:7b',
+        prompt=prompt,
+        images=[image_base64]
+    )
+    # for i in range(3):
+    #     if response1['response'] == response2['response']:
+    #         response2 = await model.generate(
+    #         model='bakllava',
+    #         prompt=prompt,
+    #         images=[image_base64]
+    #         )
+    
+    response_total=[response1['response'],response2['response'],response3['response']]
+    return response_total
+
+# 이미지 설명을 시뮬레이션합니다 (실제로는 이미지 분석 모델의 출력일 것입니다)
+# image_description = "A red apple sitting on a wooden table next to an open book."
+
+# result = simulate_image_description(image_description)
+
+
+# print(result)
