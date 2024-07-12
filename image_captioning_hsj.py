f0377271a0165cf33aa6236cb79f89099db482dc
hsjung <hyunsujung3679@gmail.com>
2024-07-12 10:07:14 +0900

    그림 설명 api 추가

--- image_captioning_hsj.py
+++ image_captioning_hsj.py
@@ -2,14 +2,9 @@ from fastapi import FastAPI
 from random import randint
 import ollama
 import torch
-from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler
+from diffusers import StableDiffusionPipeline
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
-
-# model_id = "stabilityai/stable-diffusion-2"
-# scheduler = EulerDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
-# pipe = StableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler, torch_dtype=torch.float16)
-# pipe = pipe.to("cuda")
 
 model_id = "runwayml/stable-diffusion-v1-5"
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
@@ -58,3 +53,28 @@ async def random_image_generator():
     image.save(temp_image_path)
 
     return FileResponse(temp_image_path, media_type="image/png", filename="generated_image.png")
+
+
+# STEP 1
+from fastapi import File, UploadFile
+import base64
+
+@app.post("/image_description/")
+async def simulate_image_description(file: UploadFile = File(...)):
+    
+    contents = await file.read()
+    
+    # 이미지를 base64로 인코딩
+    image_base64 = base64.b64encode(contents).decode('utf-8')
+    
+    model = ollama.Client()
+    
+    # Bakllava 모델에 이미지와 프롬프트 전송
+    prompt = "Please describe this image with different content in English three times within 30 characters in one template sentence. And don't say anything other than the three template sentences. Organize the three template sentences into numbers 1, 2, and 3, and just write the image description."
+    response = model.generate(
+        model='llava:7b',
+        prompt=prompt,
+        images=[image_base64]
+    )
+
+    return response['response']
